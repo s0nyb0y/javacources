ea7e8dee6216379065b13c4ca2b9b55d1d122b21
s0nyb0y <driveclub228@gmail.com>
2022-06-25 21:05:58 +0300

    add day 13.

--- src/day13/User.java
+++ src/day13/User.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class User {
     private String username;
-    private List<User>subscriptions;
+    private List<User> subscriptions;
 
     public User(String username) {
         this.username = username;
@@ -19,24 +19,28 @@ public class User {
     public List<User> getSubscriptions() {
         return subscriptions;
     }
-    public void subscribe(User user){
+
+    public void subscribe(User user) {
         subscriptions.add(user);
     }
-    public boolean isSubscribed(User user){
-        for (User currentUser:subscriptions) {
+
+    public boolean isSubscribed(User user) {
+        for (User currentUser : subscriptions) {
             currentUser.getUsername().equals(user.getUsername());
             return true;
         }
         return false;
     }
-    public boolean isFriends(User user){
-        if (this.isSubscribed(user) && user.isSubscribed(this)){
+
+    public boolean isFriends(User user) {
+        if (this.isSubscribed(user) && user.isSubscribed(this)) {
             return true;
         }
         return false;
     }
-    public void sendMessage(User user,String text){
-        MessageDatabase.addNewMessage(this,user,text);
+
+    public void sendMessage(User user, String text) {
+        MessageDatabase.addNewMessage(this, user, text);
     }
 
     @Override
