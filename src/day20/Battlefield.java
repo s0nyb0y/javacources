e597543fcbdd764032dc9d921dfb9bad194fb01b
s0nyb0y <driveclub228@gmail.com>
2023-03-17 18:19:00 +0300

    add day 20 правки

--- src/day20/Battlefield.java
+++ src/day20/Battlefield.java
@@ -5,108 +5,115 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Battlefield {
-    private final ShipFigure[][] field;
-    private final ShipFigure[][] monitor;
-    public static boolean winner = true;
+    private final ShipsFigure[][] field;
+    private final ShipsFigure[][] monitor;
+    public boolean winner = true;
 
-    public ShipFigure[][] getField() {
+    public Battlefield(Player player) {
+        this.field = new ShipsFigure[10][10];
+        this.monitor = new ShipsFigure[10][10];
+
+        playerField(field, monitor);
+        countShipDeck(player);
+    }
+
+    public ShipsFigure[][] getField() {
         return field;
     }
 
-    public ShipFigure[][] getMonitor() {
+    public ShipsFigure[][] getMonitor() {
         return monitor;
     }
 
-    public Battlefield(Player player) {
-        this.field = new ShipFigure[10][10];
-        this.monitor = new ShipFigure[10][10];
-
-        Map<Integer, Integer> ships = new LinkedHashMap<>();
-
+    public void playerField(ShipsFigure[][] field, ShipsFigure[][] monitor) {
         System.out.println("  0 1 2 3 4 5 6 7 8 9");
         for (int i = 0; i < field.length; i++) {
             System.out.print(i + " ");
             for (int j = 0; j < field.length; j++) {
-                System.out.print(field[i][j] = ShipFigure.EMPTY);
+                System.out.print(field[i][j] = ShipsFigure.EMPTY);
             }
             System.out.println();
         }
-
         for (int i = 0; i < monitor.length; i++) {
             for (int j = 0; j < monitor.length; j++) {
-                monitor[i][j] = ShipFigure.EMPTY;
+                monitor[i][j] = ShipsFigure.EMPTY;
             }
         }
+    }
 
-        ships.put(4, 1);
-        ships.put(3, 2);
-        ships.put(2, 3);
-        ships.put(1, 4);
-
+    public void countShipDeck(Player player) {
+        Scanner scanner = new Scanner(System.in);
+        Map<Integer, Integer> deckCount = new LinkedHashMap<>();
+        deckCount.put(4, 1);
+        deckCount.put(3, 2);
+        deckCount.put(2, 3);
+        deckCount.put(1, 4);
         int deck;
-        for (Map.Entry<Integer, Integer> ship : ships.entrySet()) {
+        int count;
+        String n = " раза";
+        String letsArrange;
+        String countDeck;
+        for (Map.Entry<Integer, Integer> entry : deckCount.entrySet()) {
             int q = 5;
-            deck = ship.getKey();
-
+            deck = entry.getKey();
+            count = entry.getValue();
             while (q > deck) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println(player.getName() + ", давай поставим " + deck + " палубный корабль");
+                if (count == 1) {
+                    letsArrange = " поставь ";
+                    countDeck = " палубный корабль";
+                } else {
+                    letsArrange = " расставь ";
+                    countDeck = " палубные корабли ";
+                }
+                if (count == 1) {
+                    System.out.println(player.getName() + "," + letsArrange + deck + countDeck);
+                } else {
+                    System.out.println(player.getName() + "," + letsArrange + deck + countDeck + entry.getValue() + n);
+                }
 
-                System.out.println("введи OX координату, значения должны быть в диапазоне 0-9: ");
-
+                System.out.println("введи OX координату, значение должно быть в диапазоне 0-9 ");
                 int x = scanner.nextInt();
                 while (x < 0 || x > 9) {
-                    System.out.println("значения должны быть в диапазоне 0-9");
+                    System.out.println("значение должно быть в диапазоне 0-9");
                     x = scanner.nextInt();
                 }
 
-                System.out.println("введи OY координату, значения должны быть в диапазоне 0-9: ");
+                System.out.println("введи OY координату, значение должно быть в диапазоне 0-9 ");
                 int y = scanner.nextInt();
                 while (y < 0 || y > 9) {
-                    System.out.println("значения должны быть в диапазоне 0-9");
+                    System.out.println("значение должно быть в диапазоне 0-9");
                     y = scanner.nextInt();
                 }
 
-                System.out.println("выбери направление корабля: \nГоризонтальное направление, цифра - 1\nВертикальное направление, цифра - 2");
+                System.out.println("выбери направление кораблей:\nГоризонтальное направление, цифра - 1\nВертикальное направление, цифра - 2");
                 int direction = scanner.nextInt();
 
                 if (!isAvailable(x, y, deck, direction, field)) {
-                    System.out.println("не все условия игры выполнены");
+                    System.out.println("не все правила игры соблюдены");
                     continue;
                 }
 
                 for (int i = 0; i < deck; i++) {
                     if (direction == 1) {
-                        field[x][y + i] = ShipFigure.SHIP;
+                        field[x][y + i] = ShipsFigure.SHIP;
                     } else {
-                        field[x + i][y] = ShipFigure.SHIP;
+                        field[x + i][y] = ShipsFigure.SHIP;
                     }
                 }
                 showField(field);
+                count--;
                 q--;
-
             }
         }
     }
 
-    public static void showField(ShipFigure[][] field) {
-        System.out.println("  0 1 2 3 4 5 6 7 8 9");
-        for (int i = 0; i < field.length; i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < field.length; j++) {
-                System.out.print(field[i][j]);
-            }
-            System.out.println();
-        }
-    }
+    public boolean isAvailable(int x, int y, int deck, int direction, ShipsFigure[][] field) {
 
-    public boolean isAvailable(int x, int y, int deck, int direction, ShipFigure[][] field) {
-        while (direction < 1 || direction > 2) {
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        while (direction < 1 || direction > 2){
             System.out.println("не корректное значение\nГоризонтальное направление, цифра - 1\nВертикальное направление, цифра - 2");
             direction = scanner.nextInt();
         }
-
         if (direction == 1) {
             if (y + deck > field.length) {
                 return false;
@@ -117,76 +124,72 @@ public class Battlefield {
                 return false;
             }
         }
-
         while (deck != 0) {
+            int xi = 0;
+            int yi = 0;
             for (int i = 0; i < deck; i++) {
-                int xi = 0;
-                int yi = 0;
                 if (direction == 1) {
                     yi = i;
                 } else {
                     xi = i;
                 }
                 if (x + 1 + xi < field.length && x + 1 + xi >= 0) {
-                    if (field[x + 1 + xi][y + yi] != ShipFigure.EMPTY) {
+                    if (field[x + 1 + xi][y + yi] != ShipsFigure.EMPTY) {
                         return false;
                     }
                 }
                 if (x - 1 + xi < field.length && x - 1 + xi >= 0) {
-                    if (field[x - 1 + xi][y + yi] != ShipFigure.EMPTY) {
+                    if (field[x - 1 + xi][y + yi] != ShipsFigure.EMPTY) {
                         return false;
                     }
                 }
                 if (y + 1 + yi < field.length && y + 1 + yi >= 0) {
-                    if (field[x + xi][y + 1 + yi] != ShipFigure.EMPTY) {
+                    if (field[x + xi][y + 1 + yi] != ShipsFigure.EMPTY) {
                         return false;
                     }
                 }
                 if (y - 1 + yi < field.length && y - 1 + yi >= 0) {
-                    if (field[x + xi][y - 1 + yi] != ShipFigure.EMPTY) {
+                    if (field[x + xi][y - 1 + yi] != ShipsFigure.EMPTY) {
                         return false;
                     }
                 }
+                deck--;
             }
-            deck--;
         }
 
         return true;
     }
 
-    public static void makeTurn(Player player, ShipFigure[][] monitor, ShipFigure[][] battlefield) {
+    public void makeTurn(Player player, ShipsFigure[][] monitor, ShipsFigure[][] field) {
         showMonitor(monitor);
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println(player.getName() + ", твой ход: ");
-            System.out.println("введи OX координату, значения должны быть в диапазоне 0-9: ");
+
+            System.out.println(player.getName() + ", твой ход\nвведи OX координату, значение должно быть в диапазоне 0-9");
             int x = scanner.nextInt();
             while (x < 0 || x > 9) {
-                System.out.println("значения должны быть в диапазоне 0-9");
+                System.out.println("значение должно быть в диапазоне 0-9");
                 x = scanner.nextInt();
             }
-
-            System.out.println("введи OY координату, значения должны быть в диапазоне 0-9: ");
+            System.out.println("введи OY координату, значение должно быть в диапазоне 0-9");
             int y = scanner.nextInt();
             while (y < 0 || y > 9) {
-                System.out.println("значения должны быть в диапазоне 0-9");
+                System.out.println("значение должно быть в диапазоне 0-9");
                 y = scanner.nextInt();
             }
             if (player.getCount() == 19) {
                 System.out.println(player.getName() + ", победил!!!");
                 winner = false;
                 break;
-
             } else {
-                if (battlefield[x][y] == ShipFigure.SHIP) {
+                if (field[x][y] == ShipsFigure.SHIP) {
                     System.out.println("Есть пробитие!");
-                    monitor[x][y] = ShipFigure.HIT;
-                    player.count();
+                    monitor[x][y] = ShipsFigure.HIT;
+                    player.countPlayer();
                     showMonitor(monitor);
-
                 } else {
-                    System.out.println("Мимо... Переход хода, противнику ");
-                    monitor[x][y] = ShipFigure.MISS;
+                    System.out.println("Мимо... Переход хода противнику");
+                    monitor[x][y] = ShipsFigure.MISS;
                     showMonitor(monitor);
                     break;
                 }
@@ -194,12 +197,23 @@ public class Battlefield {
         }
     }
 
-    public static void showMonitor(ShipFigure[][] monitor) {
+    public void showMonitor(ShipsFigure[][] monitor) {
         System.out.println("  0 1 2 3 4 5 6 7 8 9");
         for (int i = 0; i < monitor.length; i++) {
             System.out.print(i + " ");
             for (int j = 0; j < monitor.length; j++) {
                 System.out.print(monitor[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void showField(ShipsFigure[][] field) {
+        System.out.println("  0 1 2 3 4 5 6 7 8 9");
+        for (int i = 0; i < field.length; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < field.length; j++) {
+                System.out.print(field[i][j]);
             }
             System.out.println();
         }
--- src/day20/Player.java
+++ src/day20/Player.java
@@ -4,33 +4,30 @@ import java.util.Scanner;
 
 public class Player {
     private String name;
-    private int count = 0;
+    private int count;
 
-    public int getCount() {
-        return count;
+    public Player() {
+        userName();
     }
 
     public String getName() {
         return name;
     }
 
-    @Override
-    public String toString() {
-        return name;
+    public int getCount() {
+        return count;
     }
-
     public void userName(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("как вас зовут?");
         name = scanner.nextLine();
         while (name.isEmpty()){
-            System.out.println("это поле не может быть пустым");
-            System.out.println("как вас зовут?");
+            System.out.println("это поле не может быть пустым\nкак вас зовут?");
             name = scanner.nextLine();
         }
         System.out.println("привет, " + name);
     }
-    public void count(){
+    public void countPlayer(){
         count++;
     }
 }
