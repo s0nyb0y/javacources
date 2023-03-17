10ed5d6b55c774d89b252a2442d5bf67723dfa9a
s0nyb0y <driveclub228@gmail.com>
2023-03-17 18:22:52 +0300

    add day 20 правки

--- src/day20/ShipsFigure.java
+++ src/day20/ShipsFigure.java
@@ -1,11 +1,11 @@
 package day20;
 
-public enum ShipFigure {
+public enum ShipsFigure {
     EMPTY("_ ",0),SHIP("o ",1),HIT("x ",2),MISS("* ",3);
-    private String name;
-    private int value;
+    final String name;
+    final int value;
 
-    ShipFigure(String name, int value) {
+    ShipsFigure(String name, int value) {
         this.name = name;
         this.value = value;
     }
